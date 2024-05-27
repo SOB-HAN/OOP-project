66a1c15aa0223fb8709a8872cbde2db03faaeb88
PooryaM101 <poorya.mohammadi.juggernaut@gmail.com>
2024-05-28 00:37:33 +0330

    Created SignUpMenu

--- Project_Group V1.0/src/User.java
+++ Project_Group V1.0/src/User.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class User {
     //******************************************************attributes**************************************************
-    private String UserName="";
+    private String UserName=null;
+    private String password=null;
+    private String nickname=null;
+    private String Email=null;
+    private String passwordRecoveryQuestion=null;
+    private  int level;
+    private int gold;
+    private int XP;
+    ArrayList <Card> ownedCards=new ArrayList<>();
+    ArrayList <Card> selectedCards=new ArrayList<>();
     //******************************************************constructors************************************************
     //******************************************************methods*****************************************************
 }

1c6c58e53ba9f38d894f8182410ce23ca19d415a
PooryaM101 <poorya.mohammadi.juggernaut@gmail.com>
2024-05-28 12:43:35 +0330

    added isvalidpass function

---
+++ Project_Group V1.0/src/AbilityCard.java
@@ -0,0 +1,2 @@
+public class AbilityCard extends Card{
+}
--- Project_Group V1.0/src/User.java
+++ Project_Group V1.0/src/User.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class User {
     //******************************************************attributes**************************************************
-    private String UserName=null;
+    private String username=null;
     private String password=null;
     private String nickname=null;
     private String Email=null;
@@ -13,5 +13,69 @@ public class User {
     ArrayList <Card> ownedCards=new ArrayList<>();
     ArrayList <Card> selectedCards=new ArrayList<>();
     //******************************************************constructors************************************************
+
     //******************************************************methods*****************************************************
+
+    public String getUserName() {
+        return this.username;
+    }
+    public void setUserName(String UserName) {
+        username = UserName;
+    }
+    public boolean isCorrectPassword(String Password){
+        if(password!=null){
+            if(Password.equals(this.password)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void setPassword(String Password) {
+        this.password = Password;
+    }
+    public String getNickname() {
+        return this.nickname;
+    }
+    public void setNickname(String Nickname) {
+        this.nickname = Nickname;
+    }
+    public String getEmail() {
+        return this.Email;
+    }
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+    public String getPasswordRecoveryQuestion() {
+        return this.passwordRecoveryQuestion;
+    }
+    public void setPasswordRecoveryQuestion(String PasswordRecoveryQuestion) {
+        this.passwordRecoveryQuestion = PasswordRecoveryQuestion;
+    }
+    public int getLevel() {
+        return this.level;
+    }
+    public int getGold() {
+        return this.gold;
+    }
+    public int getXP() {
+        return this.XP;
+    }
+    public void setGold(int Gold) {
+        this.gold = Gold;
+    }
+    public void setLevel(int Level) {
+        this.level = Level;
+    }
+    public void setXP(int XP) {
+        this.XP = XP;
+    }
+    public ArrayList<Card> getOwnedCards() {
+        return this.ownedCards;
+    }
+    public ArrayList<Card> getSelectedCards() {
+        return this.selectedCards;
+    }
+    public boolean isValidPassword(String Password){
+        return true;
+    }
 }
